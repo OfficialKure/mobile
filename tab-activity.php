ce6f97df679af4b869f6187d8899904c1a44bcc9
Russ Steighler <russ@kurehumanite.com>
2015-12-11 18:15:00 -0800

    Create tab-activity.php

---
+++ tab-activity.php
@@ -0,0 +1,13 @@
+<ion-view title="Activity">
+  <ion-content class="has-header padding">
+  <div class="content">
+   <div class="box">
+                <div class="box-body">
+    <center>
+    	<h1> Activity </h2>
+    </center>
+    </div><!-- /.box-body -->
+                </div>
+    </div>
+  </ion-content>
+</ion-view>
