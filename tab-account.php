2a5b00efcf32989432e6c731af58263be690aec1
Russ Steighler <russ@kurehumanite.com>
2015-12-11 18:14:30 -0800

    Create tab-account.php

---
+++ tab-account.php
@@ -0,0 +1,13 @@
+<ion-view title="Account">
+  <ion-content class="has-header padding">
+  <div class="content">
+   <div class="box">
+                <div class="box-body">
+    <center>
+    	<h1> Tehe </h2>
+    </center>
+    </div><!-- /.box-body -->
+                </div>
+    </div>
+  </ion-content>
+</ion-view>
