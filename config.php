64fc89f922dfd2fa9bdbb2035ec96728fff63ffd
Russ Steighler <russ@kurehumanite.com>
2015-12-11 18:20:16 -0800

    Create config.php
    You only need this if you don't take away the php in tab-dash.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<!-- This is only if you want to create a php app. It's optional, but i used it just to test stuff on tab-dash.php. -->
+<?php
+session_start();
+mysql_connect('localhost', 'database name', 'database password');
+mysql_select_db('database name');
+?>
